ecd62f6a4006e0266942236df7ef56ca7a79eb64
Markus Szarvas <markusszar@gmail.com>
2019-10-26 17:22:42 +0200

    added basic dashboard

--- cyc/src/views/Dashboard.vue
+++ cyc/src/views/Dashboard.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>Hello this is Dashboard</div>
-</template>;
+ <md-card>
+    <md-card-header>
+      <div class="md-title">Your CO2 footprint</div>
+    </md-card-header>
+
+    <md-card-content>
+       <div id="chart">
+      <apexchart type=pie width=380 :options="chartOptions" :series="series" />
+    </div>
+    </md-card-content>
+  </md-card>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts';
+
+export default {
+  components: {
+        apexchart: VueApexCharts,
+      },
+  data: function () {
+    return {
+    series: [44, 55, 13, 43, 22],
+    chartOptions: {
+      labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: '60%'
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    }
+  } },
+}
+</script>;
