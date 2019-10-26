12918de0605b11adefb6f1b116c768856b978af6
Markus Szarvas <markusszar@gmail.com>
2019-10-26 22:19:34 +0200

    Added Achievemnts tab

---
+++ cyc/src/components/AchievementBar.vue
@@ -0,0 +1,126 @@
+<template>
+  <md-card class="card">
+    <md-card-content>
+      <div class="achievement_block">
+        <md-icon>{{ iconName }}</md-icon>
+        <div class="chart">
+          <div class="left" v-bind:class="percent"></div>
+          <div class="right" v-bind:class="rightPercent"></div>
+          <div class="indicator" v-bind:class="percent  "></div>
+        </div>
+        <div>
+          Sie hab
+        </div>
+      </div>
+    </md-card-content>
+  </md-card>
+</template>
+
+<script>
+export default {
+  props: ['iconName', 'percent', 'rightPercent'],
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  margin-bottom: 10px;
+}
+
+.achievement_block {
+  height: 50px;
+  display: flex;
+
+  .chart {
+    flex-grow: 1;
+    display: flex;
+    position: relative;
+    padding: 0 10px;
+
+    > div {
+      height: 100%;
+    }
+
+    .left {
+      background-color: red;
+    }
+    .right {
+      background-color: blue;
+    }
+
+    .indicator {
+      position: absolute;
+      top: -10px;
+      width: 0;
+      height: 0;
+      border-left: 10px solid transparent;
+      border-right: 10px solid transparent;
+      border-top: 10px solid black;
+    }
+  }
+}
+
+.w10 {
+  width: 10%;
+
+  &.indicator {
+    left: calc(40% - 2px);
+  }
+}
+.w20 {
+  width: 20%;
+
+  &.indicator {
+    left: calc(80% - 4px);
+  }
+}
+.w30 {
+  width: 30%;
+
+  &.indicator {
+    left: calc(80% - 6px);
+  }
+}
+.w40 {
+  width: 40%;
+
+  &.indicator {
+    left: calc(40% - 8px);
+  }
+}
+.w50 {
+  width: 50%;
+
+  &.indicator {
+    left: calc(50% - 10px);
+  }
+}
+.w60 {
+  width: 60%;
+
+  &.indicator {
+    left: calc(80% - 12px);
+  }
+}
+.w70 {
+  width: 70%;
+
+  &.indicator {
+    left: calc(70% - 14px);
+  }
+}
+.w80 {
+  width: 80%;
+
+  &.indicator {
+    left: calc(80% - 16px);
+  }
+}
+.w90 {
+  width:90%;
+
+  &.indicator {
+    left: calc(90% - 18px);
+  }
+}
+</style>;
--- cyc/src/views/Achievements.vue
+++ cyc/src/views/Achievements.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Hello this is Erfolge</div>
-</template>;
+  <div>
+    <AchievementBar icon-name="waves" percent="w40" right-percent="w60"/>
+    <AchievementBar icon-name="power" percent="w80" right-percent="w20"/>
+    <AchievementBar icon-name="euro" percent="w70" right-percent="w30"/>
+    <AchievementBar icon-name="directions_bike" percent="w90" right-percent="w10"/>
+  </div>
+</template>
+
+<script>
+import AchievementBar from '../components/AchievementBar.vue';
+
+export default {
+  components: {
+    AchievementBar,
+  },
+};
+</script>
