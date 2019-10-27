987dda753ea12fe158384b463a88adf2216284da
Markus Szarvas <markusszar@gmail.com>
2019-10-27 08:36:19 +0100

    Added png image because svg is broken

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,21 +15,25 @@
         <div class="md-title">Du im vergleich</div>
       </md-card-header>
 
-      <md-card-content>
-        <BalkendiagrammStartseite />
+      <md-card-content class="barChart">
+        <img src="../assets/images/Balkendiagramm.png" />
       </md-card-content>
     </md-card>
   </div>
 </template>
 
 <script>
-import BalkendiagrammStartseite from '../assets/images/Balkendiagramm_Startseite.svg';
 import Barometer from '../assets/images/Barometer.svg';
 
 export default {
   components: {
-    BalkendiagrammStartseite,
     Barometer,
   },
 };
-</script>;
+</script>
+
+<style scoped>
+.barChart {
+  width: 100%;
+}
+</style>;
